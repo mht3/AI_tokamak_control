603e30256374f9bede3354c12c5ddfdcc1aa63da
mht3 <mht3@illinois.edu>
2024-05-22 17:30:57 -0700

    finished batched training code

---
+++ generate_lyapunov_dataset.py
@@ -0,0 +1,183 @@
+import numpy as np
+from kstar_env import KSTAREnv
+import sys, os
+from common.model_structure import *
+import torch
+
+class KSTARLyapunovDataset():
+
+    def __init__(self, trajectory_length=40, N=500):
+        # AI Gym environment with data-driven KSTAR simulator
+        self.env = KSTAREnv()
+
+        # get low and high state directly from AI gym env
+        # current state consists of (9 actions, 3 0D parameters) repeated 3 times and 
+        # finally the 3 targets. Total size is 39
+        self.state_min = np.array(self.env.low_state)
+        self.state_max = np.array(self.env.high_state)
+
+        # scale standard deviation based on min/max values (used for adding noise)
+        self.scaled_sigma = np.zeros_like(self.state_max)
+        # only change current 0D parameters (indices 33-35 in state)
+        self.scaled_sigma[-6:-3] = ((self.state_max - self.state_min) / 6)[-6:-3]
+    
+        # how long of a trajectory to unroll (defaults to 40 0.1 second intervals which is a single episode)
+        self.trajectory_length = trajectory_length
+        if self.trajectory_length > 40:
+            raise ValueError('Trajectory length must be less than 40 timesteps (maximum episode length)')
+        # numer of samples in dataset (total # of different trajectories)
+        self.N = N
+
+        # initialize trained TD3 model to generate actions for dataset
+        self.load_rl_model()
+
+    def load_rl_model(self):
+        # Load RL model
+        base_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+        # RL model (stable baselines TD3)
+        rl_model_path = os.path.join(base_path, 'weights', 'rl', 'rt_control',
+                                    '3frame_v220505', 'best_model.zip')
+
+        self.rl_model = SB2_model(
+            model_path = rl_model_path, 
+            low_state = self.env.low_state, 
+            high_state = self.env.high_state, 
+            low_action = self.env.low_action, 
+            high_action = self.env.high_action, 
+            activation='relu', 
+            last_actv='tanh', 
+            norm=True, 
+            bavg=0.0
+        )
+        # initialize previous action to low action (required input to this RL model)
+        self.action = np.array(self.env.low_action)
+
+    def get_action(self, x):
+        '''
+        Take in single observation x to get action 
+        '''
+        # this RL model needs the previous action as input, so we store a class variable for action
+        self.action = self.rl_model.predict(x, yold=self.action)
+        return self.action
+    
+    def get_next_state(self, x, pi):
+        '''
+        Generates next state given current state and current action
+        x: current state
+        pi: current action based on policy
+        '''
+        # take step in environment (0.1 seconds)
+        # environment already knows about current state x so we don't need to use it here
+        observation, reward, terminated, truncated, info = self.env.step(pi)
+
+        return observation
+
+    def build(self):
+
+        initial_targets = self.load_initial_targets()
+
+        # get 3 (s, pi(s), s') pairs
+        # trajectory_size = self.trajectory_length * len(self.sta)
+        trajectories = []
+        for i in range(self.N):
+            # numpy array for initial targets
+            target = initial_targets[i, :]
+            # reset environment for new trajectory
+            self.env.reset(target_init=target)
+
+            # get current state based on initialization
+            x = self.env.get_state()
+            # list of tuples of (s, pi(s), s') pairs
+            trajectory = self.get_trajectory(x)
+            trajectories.append(trajectory)
+
+        return trajectories
+
+
+    def get_trajectory(self, x):
+        trajectory = []
+        # get action for current state x
+        pi = self.get_action(x)
+        # get next state
+        x_prime = self.get_next_state(x, pi)
+        trajectory.append((x, pi, x_prime))
+
+
+        for j in range(self.trajectory_length - 1):
+            # perturb state and use as next trajectory
+            x = x_prime + np.random.normal(0, self.scaled_sigma, len(x_prime))
+            # get action
+            pi = self.get_action(x)
+            # get next state
+            x_prime = self.get_next_state(x, pi)
+
+            # add to trajectories
+            trajectory.append((x, pi, x_prime))
+            
+
+        return trajectory
+
+        # return trajectory
+
+    def load_initial_targets(self):
+
+        # X: Nx3 numpy array of initial states
+        X = np.empty(shape=(self.N, 0))
+        for i in range(len(self.env.low_target)):
+            t_min = self.env.low_target[i]
+            t_max = self.env.high_target[i]
+            x = np.random.uniform(t_min, t_max, size=(self.N, 1))
+            X = np.concatenate([X, x], axis=1)
+        return X
+
+    def save(self, data, filename='trajectories.npz'):
+        flattened_data = self.convert_data(data)
+        np.savez_compressed(filename, *flattened_data)
+
+    def convert_data(self, data):
+        flattened = []
+        for trajectory in data:
+            for state,action,next_state in trajectory:
+                flattened.append((state,action,next_state))
+        all_states, all_actions, all_next_states = zip(*flattened)
+        return np.array(all_states), np.array(all_actions), np.array(all_next_states)
+
+def load_torch(filename):
+    '''
+    Loads trajectories.npz as a flattened torch tensor of (s, a, s')
+    '''
+    loaded = np.load(filename, allow_pickle=True)
+    loaded_data = [loaded[key] for key in loaded]
+    states, actions, next_states = loaded_data
+    dataset = np.hstack([states, actions, next_states])
+    data_tensor = torch.tensor(dataset)
+    return data_tensor.to(torch.float32)
+
+def load(filename, trajectory_length=40):
+    '''
+    Loads trajectories.npz
+    '''
+    loaded = np.load(filename, allow_pickle=True)
+    loaded_data = [loaded[key] for key in loaded]
+    states, actions, next_states = loaded_data
+    trajectories = []
+    for i in range(0, len(states), trajectory_length):
+        trajectory = [(states[j], actions[j], next_states[j]) for j in range(i, i + trajectory_length)]
+        trajectories.append(trajectory)
+    return trajectories
+
+
+if __name__ == '__main__':
+    trajectory_length = 40
+    dataset = KSTARLyapunovDataset(trajectory_length=trajectory_length, N=500)
+    trajectories = dataset.build()
+    print('##### Trajectories ######')
+    print(len(trajectories[0]))
+    dataset.save(trajectories)
+    print('##### Loaded Data ######')
+    loaded_data = load('trajectories.npz', trajectory_length)
+    print(len(loaded_data[0]))
+ 
+
+
+
--- train_model.py
+++ train_model.py
@@ -3,14 +3,20 @@ import torch
 from model import NeuralLyapunovController
 from loss import LyapunovRisk
 from matplotlib import pyplot as plt
-from lyapunov_dataset import KSTARLyapunovDataset
+from lyapunov_dataset import LyapunovDataset
+from torch.utils.data import DataLoader
+from tqdm import tqdm, trange
+
 class Trainer():
-    def __init__(self, model, lr, optimizer, loss_fn):
+    def __init__(self, model, lr, optimizer, loss_fn, loss_mode,
+                 seed=42):
         self.model = model
         self.lr = lr
         self.optimizer = optimizer
         self.lyapunov_loss = loss_fn
-    
+        self.loss_mode = loss_mode
+        self.seed = seed
+
     def get_lie_derivative(self, X, V_candidate, f):
         '''
         Calculates L_V = ∑∂V/∂xᵢ*fᵢ
@@ -36,68 +42,80 @@ class Trainer():
 
         lie_derivative = torch.diagonal((d_x @ f.t()), 0)
         return lie_derivative
+    
+    def get_approx_lie_derivative(self, V_candidate, V_candidate_next, dt=0.1):
+        '''
+        Calculates L_V = ∑∂V/∂xᵢ*fᵢ by forward finite difference
+                    L_V = (V' - V) / dt
+        '''
+        return (V_candidate_next - V_candidate) / dt
 
-    def train(self, X, x_0, epochs=2000, verbose=False, every_n_epochs=10, check_approx=False):
-        model.train()
+    def get_dataloader(self, dataset, batch_size):
+        train_loader = DataLoader(dataset, batch_size=batch_size,
+                                shuffle=True, generator=torch.Generator().manual_seed(self.seed))
+        return train_loader
+    
+    def _train(self, train_loader, x_0):
+        self.model.train()
+        total_loss = 0.
+        count = 0.
+        t_loader = tqdm(enumerate(train_loader), desc='train', total=len(train_loader), leave=False)
+        for batch_idx, data in t_loader:
+            x, pi, x_prime = data
+
+            # zero gradients
+            self.optimizer.zero_grad()
+
+            # get lyapunov function and input from model
+            V_candidate, u = self.model(x)
+            # get lyapunov function evaluated at equilibrium point
+            V_X0, u_X0 = self.model(x_0)
+
+            # get loss
+            if self.loss_mode == 'approx_dynamics':
+                # compute approximate f_dot and compare to true f
+                f_approx = approx_f_value(x, x_prime, dt=0.1)
+                L_V_approx = self.get_lie_derivative(x, V_candidate, f_approx)
+                loss = self.lyapunov_loss(V_candidate, L_V_approx, V_X0)
+
+            elif self.loss_mode == 'approx_lie':
+                # compute approximate f_dot and compare to true f
+                V_candidate_prime, u_prime = self.model(x_prime)
+                L_V_approx = self.get_approx_lie_derivative(V_candidate, V_candidate_prime, dt=0.1)
+                loss = self.lyapunov_loss(V_candidate, L_V_approx, V_X0)
+
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.item()
+            count += 1
+            t_loader.set_postfix(loss=loss.item())
+        # return avg loss over all runs
+        return total_loss / count
+    
+    def train(self, dataset, x_0, epochs=2000, batch_size=64, verbose=False, every_n_epochs=10):
         valid = False
         loss_list = []
-        approx_loss_list = []
-        if check_approx == True:
-            env = gym.make('CartPole-v1')
-
-        for epoch in range(1, epochs+1):
+        train_loader = self.get_dataloader(dataset, batch_size=batch_size)
+        pbar = trange(epochs, desc='progress')
+        for epoch in pbar:
             if valid == True:
+                # TODO Update valid once falsifier is used
                 if verbose:
                     print('Found valid solution.')
                 break
+            loss = self._train(train_loader, x_0)
 
-            # zero gradients
-            optimizer.zero_grad()
-
-            # get lyapunov function and input from model
-            V_candidate, u = self.model(X)
-            # get lyapunov function evaluated at equilibrium point
-            V_X0, u_X0 = self.model(x_0)
-            # Compute lie derivative of V : L_V = ∑∂V/∂xᵢ*fᵢ
-            f = f_value(X, u)
-            L_V = self.get_lie_derivative(X, V_candidate, f)
-            # get loss
-            loss = self.lyapunov_loss(V_candidate, L_V, V_X0)
-            
-            # compute approximate f_dot and compare to true f
-            if check_approx == True:
-                X_prime = step(X, u, env)
-                f_approx = approx_f_value(X, X_prime, dt=0.02)
-
-                # check dx/dt estimates are close
-                # epsilon for x_dot. cart velocity and angular velocity are easier to approximate than accelerations.
-                # TODO is there a better way to approximate without running throught the simulator multiple times?
-                epsilon = torch.tensor([1e-4, 10., 1e-4, 10.])
-
-                assert(torch.all(abs(f - f_approx) < epsilon))
-
-                # could replace loss function 
-                L_V_approx = self.get_lie_derivative(X, V_candidate, f_approx)
-                approx_loss = self.lyapunov_loss(V_candidate, L_V_approx, V_X0)
-                approx_loss_list.append(approx_loss.item())
-            
-
-            loss_list.append(loss.item())
-            loss.backward()
-            self.optimizer.step() 
-            if verbose and (epoch % every_n_epochs == 0):
-                print('Epoch:\t{}\tLyapunov Risk: {:.4f}'.format(epoch, loss.item()))
+            loss_list.append(loss)
+            if verbose and (epoch+1 % every_n_epochs == 0):
+                print('Epoch:\t{}\tLyapunov Risk: {:.4f}'.format(epoch+1, loss))
 
             # TODO Add in falsifier here
             # add counterexamples
-
-        if check_approx == True:
-            return loss_list, approx_loss_list
-        else:
-            return loss_list
+            metrics = {'loss' : loss}
+            pbar.set_postfix(metrics)
+        return loss_list
     
-def load_model():
-    d_in, n_hidden, d_out = 39, 6, 1
+def load_model(d_in=39, n_hidden=16, d_out=9):
     controller = NeuralLyapunovController(d_in, n_hidden, d_out)
     return controller
 
@@ -106,33 +124,51 @@ def approx_f_value(X, X_prime, dt=0.1):
     y = (X_prime - X) / dt
     return y
 
-def plot_losses(true_loss, approx_loss):
+def plot_losses(approx_dynamics_loss, approx_lie_loss):
     fig = plt.figure(figsize=(8, 6))
-    x = range(len(true_loss))
-    plt.plot(x, true_loss, label='True Loss')
-    plt.plot(x, approx_loss, label='Approximate Loss')
+    x = range(len(approx_dynamics_loss))
+    plt.plot(x, approx_dynamics_loss, label='Approximate Dynamics Loss')
+    plt.plot(x, approx_lie_loss, label='Approximate Lie Derivative Loss')
+
     plt.ylabel('Lyapunov Risk', size=16)
     plt.xlabel('Epochs', size=16)
     plt.grid()
     plt.legend()
-    plt.savefig('results/loss_comparison.png')
+    plt.savefig('loss_comparison.png')
     
 if __name__ == '__main__':
-    ### load model and training pipeline with initialized LQR weights ###
-    model = load_model()
     lr = 0.01
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    batch_size=500
+    epochs=50
     loss_fn = LyapunovRisk(lyapunov_factor=1., lie_factor=1., equilibrium_factor=1.)
-    trainer = Trainer(model, lr, optimizer, loss_fn)
 
-    ### Generate random training data ###
+    state_dim = 39
+    action_dim = 9
+    
+    ### Load training data ###
     print('Loading dataset...')
-    data = KSTARLyapunovDataset.load('trajectories.npz')
-    print(len(data), len(data[0]))
-    # TODO: Find equilibrium for KSTAR env
-    X_0 = torch.zeros(39)
+    lyapunov_dataset = LyapunovDataset(filename='trajectories.npz', 
+                                       state_dim=state_dim, action_dim=action_dim)
 
-    # ### Start training process ##
-    # approx = True # calculate lie derivative when system dynamics are unknown (this model compares the approximate f to the ground truth)
-    # true_loss, approx_loss = trainer.train(X, X_0, epochs=200, verbose=True, check_approx=approx)
-    # plot_losses(true_loss, approx_loss)
+    # TODO: Find equilibrium for KSTAR env
+    X_0 = torch.zeros(state_dim)
+
+    print("Training with approx dynamics loss...")
+    d_in, n_hidden, d_out = 39, 16, 9
+
+    model_1 = load_model(d_in, n_hidden, d_out)
+    optimizer_1 = torch.optim.Adam(model_1.parameters(), lr=lr)
+    trainer_1 = Trainer(model_1, lr, optimizer_1, loss_fn, loss_mode='approx_dynamics')
+    # calculate lie derivative when system dynamics are unknown (this model compares the approximate f to the ground truth)
+    approx_dynamics_loss = trainer_1.train(lyapunov_dataset, X_0, epochs=epochs,
+                                           batch_size=batch_size, verbose=True)
+
+    print("Training with approx lie derivative loss...")
+    model_2 = load_model()
+    optimizer_2 = torch.optim.Adam(model_2.parameters(), lr=lr)
+    trainer_2 = Trainer(model_2, lr, optimizer_2, loss_fn, loss_mode='approx_lie')
+    # calculate lie derivative when system dynamics are unknown (this model compares the approximate f to the ground truth)
+    approx_lie_loss = trainer_2.train(lyapunov_dataset, X_0, epochs=epochs,
+                                      batch_size=batch_size, verbose=True)
+    
+    plot_losses(approx_dynamics_loss, approx_lie_loss)
